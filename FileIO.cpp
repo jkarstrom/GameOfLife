f51bd089413548cadeb7522641c188834d9385cc
jkarstrom <44649059+jkarstrom@users.noreply.github.com>
2020-03-08 14:24:37 -0700

    test

---
+++ FileIO.cpp
@@ -0,0 +1,22 @@
+#include "FileIO.h"
+
+FileIO::FileIO()
+{
+  //default constructor
+}
+
+FileIO::FileIO()
+{
+
+}
+
+
+
+
+
+void FileIO::printGenNumbers()
+{
+
+}
+ofstream newfile;
+newfile.open ("Karstrom.txt");
---
+++ MirrorMode.cpp
@@ -0,0 +1,16 @@
+#include "MirrorMode.h"
+
+MirrorMode::MirrorMode()
+{
+  //default constructor
+}
+
+MirrorMode::MirrorMode()
+{
+
+}
+
+int MirrorMode::checkNeighbors()
+{
+  
+}
